c455977e12532d19eea5b25e22116af4ffce58c8
guilhermehccruz <guihccruz60@gmail.com>
2022-06-03 00:54:20 -0300

    Completed

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,13 +1,27 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 
-import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+import { ListAllUsersUseCase } from './ListAllUsersUseCase';
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+	constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+	handle(request: Request, response: Response): Response {
+		let { user_id } = request.headers;
+
+		if (!user_id) {
+			return response.status(400).send();
+		}
+
+		user_id = String(user_id);
+
+		try {
+			const users = this.listAllUsersUseCase.execute({ user_id });
+
+			return response.json(users);
+		} catch (error) {
+			return response.status(400).json({ error: error.message });
+		}
+	}
 }
 
 export { ListAllUsersController };
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -1,16 +1,20 @@
-import { User } from "../../model/User";
-import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { User } from '../../model/User';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
 
 interface IRequest {
-  user_id: string;
+	user_id: string;
 }
 
 class TurnUserAdminUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+	constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User {
-    // Complete aqui
-  }
+	execute({ user_id }: IRequest): User {
+		let user = this.usersRepository.findById(user_id);
+
+		user = this.usersRepository.turnAdmin(user);
+
+		return user;
+	}
 }
 
 export { TurnUserAdminUseCase };
